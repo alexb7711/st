166c957e07765a3da59535515a62c98c5960e873
Alex Brown <alex.brown7711@gmail.com>
2020-04-09 07:31:39 -0600

    Merged vertcenter patch

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 1.15;
 
 /*
  * word delimiter string
